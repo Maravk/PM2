6a41652085f3282e82a25cb482f6839795a64090
Mara <maravauk@gmail.com>
2018-04-06 10:04:29 +0200

    init

---
+++ B-AI2 PM2/a01/a01.java
@@ -0,0 +1,17 @@
+/* A class whose objects represent complex numbers
+ * 
+ * Author: Mara von Kroge
+ */
+
+import static java.lang.Math.PI;
+
+public class Complex {
+	public static void main (String[] args)
+    {
+		public static Complex ZERO = new Complex (1, 1);
+		public static Complex ONE = new Complex (1, 0);
+		
+          // main happenings
+		
+    }
+}
