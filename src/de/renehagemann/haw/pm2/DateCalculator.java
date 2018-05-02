ef37d77d2251738176e9260801f78aedb6615560
Mara <Mara@Mara-PC>
2018-05-02 23:47:07 +0200

    1.1 done

--- src/de/renehagemann/haw/pm2/DateCalculator.java
+++ src/de/renehagemann/haw/pm2/DateCalculator.java
@@ -13,9 +13,6 @@ import java.time.temporal.TemporalAdjusters;
 import java.time.DayOfWeek;
 
 
-/* Können jetzt wenigstens einen begrenzten Stream aufrufen,
- * jetzt muss nur noch das Ergebnis gefiltert werden können. 
- */
 
 public class DateCalculator {
 
@@ -24,39 +21,42 @@ public class DateCalculator {
     	/*
     	 * Aufgabe 1.1	
     	 */
+    	System.out.println("Aufgabe 1.1");
     	
-    	// gesamter Stream wird geprinted
     	LocalDate firstofmay = LocalDate.of(1889,5,1);
-        System.out.println( getDays(firstofmay, (2100-1889) ));
-        Stream<LocalDate> allFirstMays = getDays(firstofmay, (2100-1889));
-        Stream<LocalDate> tuesdayFirstMays = 
-        allFirstMays.filter(n-> n.getDayOfWeek.name == "TUESDAY");
+    	
+//    	System.out.println(firstofmay.getDayOfWeek());
+//    	
+//        System.out.println( getDays(firstofmay, (2100-1889) ));
+        Stream<LocalDate> allFirstMays = getDays(firstofmay, (2100-1888));
+        List<LocalDate> tuesdayFirstMays = 
+        allFirstMays.filter(n-> n.getDayOfWeek() == DayOfWeek.TUESDAY)
+        .collect(Collectors.toList());
         
-        System.out.println(tuesdayFirstMays);
+        System.out.println("Jeder erste Mai zwischen 1889 und 2100 der "
+        + "auf einen Dienstag fällt: " + tuesdayFirstMays);
         
         /* 
          * Aufgabe 1.3.1
          */
-        
+        System.out.println("\n\nAufgabe 1.3.1\n");
         firstofmay = LocalDate.of(2018, 5, 1);
-    	System.out.println("Aufgabe 1.3.1");
         System.out.println("The next Sunday after your date " + 
         firstofmay + ": " +nextSunday(firstofmay));
         
         /*
          * Aufgabe 1.3.2
          */
-        
-        System.out.println("Aufgabe 1.3.2");
+        System.out.println("\n\nAufgabe 1.3.2\n");
         int leapyear = firstofmay.getYear();
         System.out.println("The next Leapyear: " + nextLeapYear(leapyear));
         
         /*
          * Aufgabe 1.3.3
          */
-        System.out.println("Aufgabe 1.3.3");
+        System.out.println("\n\nAufgabe 1.3.3\n");
         System.out.println("Das Datum 42 Tage nach dem 1. Mai ist: " 
-        + firstofmay.plusDays(42));;
+        + firstofmay.plusDays(42));
        
     }
     public static int nextLeapYear(int year) {
@@ -77,7 +77,7 @@ public class DateCalculator {
     	return date.with(TemporalAdjusters.next(DayOfWeek.SUNDAY));
     }
  
-    //Stream of dates with 1 Year difference
+    //Stream of the same date of different years
     public static Stream<LocalDate> getDays(LocalDate start, int days)
     {
         return Stream.iterate(start, date -> date.plusYears(1))
